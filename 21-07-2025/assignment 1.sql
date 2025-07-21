371b518e58cccf6bb2de9c0cdd1c7e89bb3dd95f
shruthi-csd <rajashruthika3104@gmail.com>
2025-07-21 10:58:13 +0530

    assignment 1.sql

--- 21-07-2025/assignment 1.sql
+++ 21-07-2025/assignment 1.sql
@@ -24,42 +24,58 @@ INSERT INTO departments VALUES
 (2, 'Marketing', 'Mumbai'),
 (3, 'HR', 'Delhi'),
 (4, 'Sales', 'Chennai');
+
 select * from employees;
+
 select * from departments;
+
 SELECT emp_name, salary FROM employees;
+
 SELECT * FROM employees
 WHERE salary > 40000;
+
 SELECT * FROM employees
 WHERE age BETWEEN 28 AND 32;
+
 SELECT * FROM employees
 WHERE department != 'HR';
+
 SELECT * FROM employees
 ORDER BY salary DESC;
+
 SELECT COUNT(*) AS total_employees FROM employees;
+
 SELECT * FROM employees
 ORDER BY salary DESC
 LIMIT 1;
+
 SELECT e.emp_name, d.location
 FROM employees e
 JOIN departments d ON e.department = d.dept_name;
+
 SELECT d.dept_name, COUNT(e.emp_id) AS employee_count
 FROM departments d
 LEFT JOIN employees e ON d.dept_name = e.department
 GROUP BY d.dept_name;
+
 SELECT department, AVG(salary) AS avg_salary
 FROM employees
 GROUP BY department;
+
 SELECT d.dept_name
 FROM departments d
 LEFT JOIN employees e ON d.dept_name = e.department
 WHERE e.emp_id IS NULL;
+
 SELECT department, SUM(salary) AS total_salary
 FROM employees
 GROUP BY department;
+
 SELECT department, AVG(salary) AS avg_salary
 FROM employees
 GROUP BY department
 HAVING AVG(salary) > 45000;
+
 SELECT emp_name, department
 FROM employees
 WHERE salary > 50000;
