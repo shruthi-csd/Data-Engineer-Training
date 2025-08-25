ee18810d0657aa9bc706b90126413368c13b410f
shruthi-csd <rajashruthika3104@gmail.com>
2025-08-25 12:51:50 +0530

    Add files via upload

---
+++ 25-08-2025/Task1/data pipelines/fetch_data.py
@@ -0,0 +1,14 @@
+import json
+
+def fetch():
+    data = {"students": [
+        {"id": 1, "name": "Abhinav", "marks": 78},
+        {"id": 2, "name": "Priya", "marks": 85},
+        {"id": 3, "name": "Rahul", "marks": 92},
+    ]}
+    with open("raw_data.json", "w") as f:
+        json.dump(data, f)
+    print("✅ Raw data fetched and saved to raw_data.json")
+
+if __name__ == "__main__":
+    fetch()
